d375821d26fb845d1f4912ca1915aa6007b21f13
Steven-Grecescu <steven.grecescu@gmail.com>
2024-01-09 00:30:53 +0100

    Fin exo liste

--- Exo js/ListeDesTâches/script.js
+++ Exo js/ListeDesTâches/script.js
@@ -1,15 +1,36 @@
-function ajouterTacher(){
-    let NouvelleTache = window.prompt("Quel est votre tache?");
-    if (NouvelleTache === ''){
-        window.confirm("Veuillez saisir votre tache");
-    }else{
-        document.getElementById("UL").innerHTML += "<li>" + NouvelleTache + "</li>";
-        document.innerHTML += "<span >" + "Modifier" + "</span>";
-        document.innetHTML += "<span>" + "Supprimer" + "</span>";
-    }
+function ajouterTache() {
+  let tacheId = 0;
+  const TACHE = document.querySelector("#nouveauToDo").value;
+  if (TACHE != "") {
+    const SPAN = document.createElement("span");
+    const DIV = document.createElement("div");
 
+    SPAN.textContent = TACHE;
+
+    const MODIFIER = document.createElement("button");
+    MODIFIER.textContent = "Modifier";
+    MODIFIER.classList.add("Modifier");
+    MODIFIER.setAttribute("onclick", "modifierTache(" + tacheId + ")");
+
+    const EFFACER = document.createElement("button");
+    EFFACER.textContent = "Effacer";
+    EFFACER.classList.add("Effacer");
+    EFFACER.setAttribute("onclick", "supprimerTache(" + tacheId + ")");
+
+    DIV.appendChild(SPAN);
+    DIV.appendChild(MODIFIER);
+    DIV.appendChild(EFFACER);
+    DIV.classList.add("tache" + tacheId);
+    tacheId++;
+    document.querySelector("#listeTaches").appendChild(DIV);
+  }
 }
 
-// supprimerTache(){
-
-// }
+function modifierTache(id) {
+  const TACHE = document.querySelector(".tache" + id).firstChild.textContent;
+  let modification = prompt("Modifier la tâche", TACHE);
+  document.querySelector(".tache" + id).firstChild.textContent = modification;
+}
+function supprimerTache(id) {
+  document.querySelector(".tache" + id).remove();
+}
