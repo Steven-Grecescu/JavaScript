9e4f2fbd3b924b6efcd6002aa8e0d98be5c71aca
Steven-Grecescu <steven.grecescu@gmail.com>
2024-01-09 16:14:28 +0100

    Fin calculatrice

--- Exo js/calculatrice/script.js
+++ Exo js/calculatrice/script.js
@@ -1,23 +1,64 @@
-        function display(val) { 
-            document.getElementById("resultat").value += val 
-        } 
-  
-        function evenementTouche(event) { 
-            if (event.key == "1" || event.key == "2" || event.key == "3" || event.key == "4" || event.key == "5" || event.key == "6" || event.key == "7" || event.key == "8" || event.key == "9" || event.key == "0" || event.key == "/" || event.key == "*" || event.key == "+" || event.key == "-" || event.key == "." || event.key == "=" )
-                document.getElementById("resultat").value += event.key; 
-        } 
-    
-        function resoudre() { 
-            let x = document.getElementById("resultat").value ;
-            let y = math.evaluate(x) ;
-            document.getElementById("resultat").value = y ;
-        } 
-  
-        function effacer() { 
-            document.getElementById("resultat").value = "" ;
-        } 
+function display(val) {
+  document.getElementById("resultat").value += val;
+}
 
-        function darkMode() {
-            var element = document.body;
-            element.classList.toggle("dark-mode");
-          } 
+function evenementTouche(event) {
+  switch (event.key) {
+    case "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9" || "0" || "/" || "*" || "+" || "-" || ".":
+      document.getElementById("resultat").value += event.key;
+      break;
+
+    case "=":
+      resoudre();
+      break;
+    case "Backspace":
+      effacerUn();
+      break;
+    case "Delete" || event.key == "c" || event.key == "C":
+      effacer();
+      break;
+    case "²":
+      resoudreCarrer();
+      break;
+  }
+}
+
+function effacerUn() {
+  let verif = document.getElementById("resultat").value;
+  document.getElementById("resultat").value = verif.substring(
+    0,
+    verif.length - 1
+  );
+}
+
+function resoudre() {
+  let x = document.getElementById("resultat").value;
+  let y = math.evaluate(x);
+  document.getElementById("resultat").value = y;
+}
+
+function resoudreRacineCarrer() {
+  let x = document.getElementById("resultat").value;
+  let y = math.sqrt(x);
+  document.getElementById("resultat").value = y;
+}
+
+function resoudreCarrer() {
+  let x = document.getElementById("resultat").value;
+  let y = x * x;
+  document.getElementById("resultat").value = y;
+}
+
+function resoudreInverse() {
+  let x = document.getElementById("resultat").value;
+  let y = 1 / x;
+  document.getElementById("resultat").value = y;
+}
+function effacer() {
+  document.getElementById("resultat").value = "";
+}
+
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
